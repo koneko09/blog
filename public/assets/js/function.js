2b72b7908e6f77143c11129b8c6e18a989bc5bde
NguyennMinhh <minhnguyen8762382004@gmail.com>
2024-12-06 06:56:37 +0700

    commented console log

--- public/assets/js/function.js
+++ public/assets/js/function.js
@@ -5,15 +5,15 @@ function changeTheme() {
         if (element.classList.contains('white-theme')) {
             localStorage.setItem('theme', 'dark-theme');
             document.getElementById('themeButton').innerHTML = "🌞";
-            console.log("Check if localStorage set dark-theme yet? => ", localStorage.getItem('theme'))
+            // console.log("Check if localStorage set dark-theme yet? => ", localStorage.getItem('theme'))
         } else if (element.classList.contains('dark-theme')) {
             localStorage.setItem('theme', 'white-theme');
             document.getElementById('themeButton').innerHTML = "🌚";
-            console.log("Check if localStorage set white-theme yet? => ", localStorage.getItem('theme'))
+            // console.log("Check if localStorage set white-theme yet? => ", localStorage.getItem('theme'))
         } else {
             localStorage.setItem('theme', 'white-theme');
             document.getElementById('themeButton').innerHTML = "🌚";
-            console.log("Check if localStorage set white-theme yet? => ", localStorage.getItem('theme'))
+            // console.log("Check if localStorage set white-theme yet? => ", localStorage.getItem('theme'))
         }
     });
 
@@ -26,7 +26,7 @@ function applySavedTheme() {
         element.classList.remove('white-theme', 'dark-theme');
         element.classList.add(savedTheme);
 
-        console.log("in applySavedTheme, savedTheme = ", savedTheme)
+        // console.log("in applySavedTheme, savedTheme = ", savedTheme)
 
         if (savedTheme === "white-theme") {
             document.getElementById('themeButton').innerHTML = "🌚";
